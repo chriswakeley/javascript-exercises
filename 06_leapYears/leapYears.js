dd8abcb9b27f93039c07c6141587334f8f21fef4
chriswakeley <chriswakeley@gmail.com>
2023-04-04 14:51:03 -0400

    Solve 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if(typeof(inputYear) !== "number" || inputYear < 0){
+        return 'ERROR';
+    }
+    if(inputYear % 400 === 0 || (inputYear % 100 !== 0 && inputYear % 4 === 0)){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
