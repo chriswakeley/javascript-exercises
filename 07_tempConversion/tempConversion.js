5c856a7f08152ede069c319e637b34b04ca4bf6a
chriswakeley <chriswakeley@gmail.com>
2023-04-04 15:11:05 -0400

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  return +(((degreesF - 32)*(5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  return +(((degreesC * (9/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
