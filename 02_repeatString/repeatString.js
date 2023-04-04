39dc8ebbfab3a54bb1e0e6d927df81ce989a81fd
chriswakeley <chriswakeley@gmail.com>
2023-04-04 12:25:07 -0400

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numRepeats) {
+    if(numRepeats < 0){
+        return 'ERROR';
+    }
+    let repeatedString = "";
+    for (let i=0; i<numRepeats; i++){
+        repeatedString += inputString;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
