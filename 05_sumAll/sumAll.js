4ab5383e861e6dcde42a26e8499a333e5759ec22
chriswakeley <chriswakeley@gmail.com>
2023-04-04 14:36:17 -0400

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(inputBound1, inputBound2) {
+    if(Math.min(inputBound1, inputBound2) < 0 || 
+        typeof(inputBound1) !== "number" || 
+        typeof(inputBound2) !== "number" ){
+        return 'ERROR';
+    }
+    let sum = 0;
+    for(let i = Math.min(inputBound1, inputBound2); i<Math.max(inputBound1, inputBound2) + 1; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
